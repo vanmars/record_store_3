5e17b45a0809fc2e72f83aaa99c06ba5e6894eb6
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-29 12:04:41 -0700

    add search and update functionality
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

---
+++ app.rb
@@ -0,0 +1,61 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/album')
+require('pry')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums/new') do
+  erb(:new_album)
+end
+
+get('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
+end
+
+get('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+end
+
+post('/albums') do
+  name = params[:album_name]
+  year = params[:album_year]
+  genre = params[:album_genre]
+  artist = params[:album_artist]
+  album = Album.new(name, nil, year, genre, artist)
+  album.save()
+  @albums = Album.all()
+  erb(:albums)
+end
+
+post('/albums/search') do
+  name = params[:search]
+  @results = Album.search(name)
+  erb(:search_results)
+end
+
+patch('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.update(params[:name], params[:year], params[:genre], params[:artist])
+  @albums = Album.all
+  erb(:albums)
+end
+
+delete('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.delete()
+  @albums = Album.all
+  erb(:albums)
+end
+
---
+++ lib/album.rb
@@ -0,0 +1,61 @@
+class Album
+  attr_reader :id, :name, :year, :genre, :artist #Our new save method will need reader methods.
+
+  @@albums = {}
+  @@total_rows = 0 # We've added a class variable to keep track of total rows and increment the value when an ALbum is added.
+
+  def initialize(name, id, year, genre, artist) # We've added id as a second parameter.
+    @name = name
+    @id = id || @@total_rows += 1  # We've added code to handle the id.
+    @year = year
+    @genre = genre
+    @artist = artist
+  end
+
+  def self.all
+    @@albums.values()
+  end
+
+  def self.search(name)
+    results = []
+    self.all.each do |album|
+      if album.name == name
+        results.push(album)
+      end
+    end
+    results
+  end
+
+  # def self.searh(name)
+  #   result = self.all.select {|album| album.name.include?(name)}
+  #   return result
+  # end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id, self.year, self.genre, self.artist)
+  end
+
+  def ==(album_to_compare)
+    self.name() == album_to_compare.name()
+  end
+
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@albums[id]
+  end
+
+  def update(name, year, genre, artist)
+    @name = name
+    @year = year
+    @genre = genre
+    @artist = artist
+  end
+
+  def delete
+    @@albums.delete(self.id)
+  end
+end
