f5540bf721bb31665d740a2c4bb3c12ba0768f3c
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-01 21:37:55 -0700

    Eliminate unnecessary lines

--- app.rb
+++ app.rb
@@ -65,13 +65,11 @@ delete('/albums/:id') do
   erb(:albums)
 end
 
-# Get the detail for a specific song such as lyrics and songwriters.
 get('/albums/:id/songs/:song_id') do
   @song = Song.find(params[:song_id].to_i())
   erb(:song)
 end
 
-# Post a new song. After the song is added, Sinatra will route to the view for the album the song belongs to.
 post('/albums/:id/songs') do
   @album = Album.find(params[:id].to_i())
   song = Song.new(params[:song_name], @album.id, nil)
@@ -79,7 +77,7 @@ post('/albums/:id/songs') do
   erb(:album)
 end
 
-# Edit a song and then route back to the album view.
+
 patch('/albums/:id/songs/:song_id') do
   @album = Album.find(params[:id].to_i())
   song = Song.find(params[:song_id].to_i())
@@ -87,7 +85,7 @@ patch('/albums/:id/songs/:song_id') do
   erb(:album)
 end
 
-# Delete a song and then route back to the album view.
+
 delete('/albums/:id/songs/:song_id') do
   song = Song.find(params[:song_id].to_i())
   song.delete
